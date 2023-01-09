ad3514036925b60e32f7465f5602070643d9c3a7
Pavel <gamenimsi@gmail.com>
2023-01-09 18:03:50 +0300

    up

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
 import Layout from "../components/Layout";
 
-export default function Home({posts}) {
+export default function Home() {
   return (
     <Layout title='WARCRAFT'>
-      {/*<h1>Последние новости</h1>*/}
-      {/*<div className="posts">*/}
-      {/*  {posts.map(post => (*/}
-      {/*    <BlogItem*/}
-      {/*      key={post.id}*/}
-      {/*      title={post.title}*/}
-      {/*      description={post.body}*/}
-      {/*      id={post.id} />*/}
-      {/*  ))}*/}
-      {/*</div>*/}
+    
     </Layout>
   )
 }
-
-// export const getServerSideProps = async() => {
-//
-//   const data = await fetch(`${process.env.API_URL}/posts`)
-//   const posts = await data.json()
-//
-//   return {
-//     props: { posts },
-//   }
-// }
